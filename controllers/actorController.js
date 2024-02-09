3551a15a3bd5307837f721c879633d338275b911
Snus292 <georgiigiblov@gmail.com>
2024-02-09 14:33:46 +0200

    добавлен еще файл контролеера actor, нужно доделать

--- controllers/actorController.js
+++ controllers/actorController.js
@@ -6,5 +6,21 @@ const getAllActors = async (req,res)=>{
         //вызов сервиса для получения всех актеров из базы данных
         const actors= await ActorService.getAllActors();
         //отправка ответа с массивом актеров в формате JSON
+        res.jso(actors);
+    }catch(error){
+        //в случае ошибки вывожим ее в консоль  иотправляем статус 500 с смс
+        console.error(500).send("Internal Server Error");
+    }
+};
+
+//получение фильмов с участием определенного актера
+const getMoviesByActor=async(req,res)=>{
+    //извлекаем имя актера из параметров запроса
+    const {actor}= req.params;
+    try{
+        //вызов сервиса для получения фильмов с участие определенноого актера
+        const movies =await ActorService.getMoviesByActor(actor);
+        //отправка ответа с массивом фильмаов в формате JSON
+        res.json(movies);
     }
 }
